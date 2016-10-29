2b8f80f1865fcc8ae1d27ad10c8e703e33bcde21
Undin <a.pendryak@yandex.ru>
2016-10-30 01:37:05 +0300

    add cmd args

--- src/main/kotlin/com/warrior/series_crawler/main.kt
+++ src/main/kotlin/com/warrior/series_crawler/main.kt
@@ -4,14 +4,31 @@ import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.warrior.series_crawler.crawlers.LostFilmCrawler
 import com.warrior.series_crawler.crawlers.NewStudioCrawler
+import org.apache.commons.cli.DefaultParser
+import org.apache.commons.cli.HelpFormatter
+import org.apache.commons.cli.Options
 import java.io.File
 
 /**
  * Created by warrior on 10/29/16.
  */
 fun main(args: Array<String>) {
+    val parser = DefaultParser()
+
+    val options = Options()
+    options.addOption("s", "settings", true, "path to settings file")
+    options.addOption("h", "help", false, "show this help")
+
+    val line = parser.parse(options, args)
+    if (line.hasOption("h")) {
+        val formatter = HelpFormatter()
+        formatter.printHelp("java -jar series-crawler-jarfile.jar [options...]", options)
+        return
+    }
+    val settingsFile = line.getOptionValue("s")
+
     val mapper = ObjectMapper()
-    val settings: Map<String, List<String>> = mapper.readValue(File("./settings.json"),
+    val settings: Map<String, List<String>> = mapper.readValue(File(settingsFile),
             object : TypeReference<Map<String, List<String>>>() {})
 
     for ((k, v) in settings) {
