75494c3af90ffcd76c76476a4d74697c0fb3267c
Arseniy Pendryak <a.pendryak@yandex.ru>
2017-03-06 00:15:27 +0800

    use telegram api. reply on each message

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,45 +1,47 @@
 extern crate dotenv;
 extern crate reqwest;
 extern crate server;
+extern crate threadpool;
 
 use dotenv::dotenv;
 use reqwest::{Client, StatusCode};
-use server::entities::{UpdateResponse, Update};
-use std::env;
+use server::telegram_api::{Chat, Message, TelegramApi, Update, UpdateResponse, User};
+use std::{env, thread};
+use std::sync::Arc;
+use threadpool::ThreadPool;
 
 fn main() {
     dotenv().ok();
     let bot_token = env::var("BOT_TOKEN").expect("BOT_TOKEN variable must be set");
-    let base_url = format!("https://api.telegram.org/bot{}/getUpdates?timeout=20&limit=1&offset=", bot_token);
 
     let client: Client = reqwest::Client::new().unwrap();
+    let api: Arc<TelegramApi> = Arc::new(TelegramApi::new(client, bot_token));
     let mut update_id = 0i32;
 
+    let thread_pool = ThreadPool::new_with_name("thread-pool".into(), 4);
     loop {
-        let url = base_url.to_string() + &update_id.to_string();
-        println!("--> {}", url);
-
-        match client.get(&url).send() {
-            Ok(mut response) => {
-                println!("response status: {}", response.status());
-                if *response.status() == StatusCode::Ok {
-                    if let Ok(update_response) = response.json::<UpdateResponse>() {
-                        let updates: Vec<Update> = update_response.result;
-                        if updates.len() > 0 {
-                            let update = &updates[0];
-                            update_id = update.update_id + 1;
-                            if let Some(ref message) = update.message {
-                                println!("{:?}", message)
-                            }
-                        } else {
-                            println!("no updates");
-                        }
-                    } else {
-                        println!("can't parse response body");
-                    }
+        match api.get_updates(20, 1, update_id) {
+            Ok(update_response) => {
+                let updates: Vec<Update> = update_response.result;
+                for update in updates {
+                    update_id = update.update_id + 1;
+                    let api_copy = api.clone();
+                    thread_pool.execute(move || process_update(update, api_copy));
                 }
-            },
-            Err(error) => println!("{}", error)
+            }
+            Err(error) => println!("{}", error),
+        }
+    }
+}
+
+fn process_update(update: Update, api: Arc<TelegramApi>) {
+    for message in update.message {
+        match message {
+            Message { chat: Chat { id: chat_id, .. }, from: Some(user), text: Some(user_text), .. } => {
+                println!("{}", user_text);
+                api.send_message(chat_id, "Hello!");
+            }
+            _ => println!("skip message"),
         }
     }
 }
